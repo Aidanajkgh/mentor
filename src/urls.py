94093bb6d4114f631d22f1994e4750d00024859d
unknown <Straykids>
2023-08-28 18:48:16 +0600

    added user authentication

--- src/urls.py
+++ src/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ads/', include('ads.urls'))
+    path('ads/', include('ads.urls')),
+    path('user/', include('users.urls'))
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
